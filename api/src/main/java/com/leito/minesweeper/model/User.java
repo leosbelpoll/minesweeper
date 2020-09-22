3e9de14ce6ddad5cef835b1d3c1933e2607c0571
Leosbel Poll Sotomayor <leitopoll25@gmail.com>
2020-09-22 17:56:10 -0300

    API: add User model

---
+++ api/src/main/java/com/leito/minesweeper/model/User.java
@@ -0,0 +1,19 @@
+package com.leito.minesweeper.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(unique = true)
+    private String username;
+}
