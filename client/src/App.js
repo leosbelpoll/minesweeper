83bb300629fbf44cdb3ff7ee9a1aaf9cc7db564a
Leosbel Poll Sotomayor <leitopoll25@gmail.com>
2020-09-23 22:05:26 -0300

    Client: Add redux to manage the app state

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,14 @@
 import React from "react";
+import { Provider } from "react-redux";
+
+import store from "redux/store";
 
 function App() {
-    return <div></div>;
+    return (
+        <Provider store={store}>
+            <div></div>
+        </Provider>
+    );
 }
 
 export default App;
