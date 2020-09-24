7f991b82f12d262a74647de389fd834198a2d936
Leosbel Poll Sotomayor <leitopoll25@gmail.com>
2020-09-23 22:53:42 -0300

    Client: add basic router

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,12 @@ import React from "react";
 import { Provider } from "react-redux";
 
 import store from "redux/store";
+import Routes from "components/Routes";
 
 function App() {
     return (
         <Provider store={store}>
-            <div></div>
+            <Routes />
         </Provider>
     );
 }
---
+++ client/src/components/Routes/Routes.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+
+const Routes = () => {
+    return (
+        <div className="app-component">
+            <Router>
+                <h1>Router working!</h1>
+            </Router>
+        </div>
+    );
+};
+
+export default Routes;
